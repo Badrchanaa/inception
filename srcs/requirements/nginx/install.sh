604f04bbd9065e0e1efa2b1132d9d2a048707926
Badr Chanaa <badrchanaa@gmail.com>
2025-08-23 14:14:49 +0100

    wordpress

--- srcs/requirements/nginx/install.sh
+++ srcs/requirements/nginx/install.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
-apk update
-apk add --no-cache nginx
-adduser -D -g 'www' www
-mkdir /www
-chown -R www:www /var/lib/nginx
-chown -R www:www /www
 
 mkdir /usr/share/nginx/html
 cat << EOF > /usr/share/nginx/html/index.html
