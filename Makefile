53b1bf793671d5dc73e5fa79ce960fc8b7619b54
bchanaa <bchanaa@inception.myguest.virtualbox.org>
2025-08-25 00:39:46 +0100

    wp user

---
+++ Makefile
@@ -0,0 +1,33 @@
+COMPOSE_FILE=srcs/docker_compose.yml
+ENV_FILE=srcs/.env
+
+NAME=WORDPRESS_APP
+
+SHELL := /bin/bash
+.ONESHELL:
+
+all: $(NAME)
+
+$(NAME): $(COMPOSE_FILE) $(ENV_FILE)
+	docker compose -f $(COMPOSE_FILE) up -d
+clean:
+	docker compose -f $(COMPOSE_FILE) down
+
+re: fclean $(NAME)
+
+fclean: clean
+	source srcs/.env
+	if [[ -v DB_VOLUME_PATH ]] && [[ -v WP_VOLUME_PATH ]]; then
+		- sudo chown -R $$(whoami):$$(whoami) $$DB_VOLUME_PATH
+		- sudo chmod -R u+rwX "$$DB_VOLUME_PATH"
+		- sudo rm -rf "$$DB_VOLUME_PATH/"*
+		- sudo chown -R $$(whoami):$$(whoami) $$WP_VOLUME_PATH
+		- sudo chmod -R u+rwX "$$WP_VOLUME_PATH"
+		- sudo rm -rf "$$WP_VOLUME_PATH/"*
+		- docker stop $$(docker ps -aq)
+		- docker rm $$(docker ps -aq)
+		- docker rmi -f $$(docker images -aq)
+	fi
+	# - docker system prune -a --volumes -f
+
+.PHONY: fclean re clean
--- srcs/requirements/wordpress/install.sh
+++ srcs/requirements/wordpress/install.sh
@@ -4,19 +4,14 @@ set -e
 if [ -z $(ls -A "$WP_DATADIR") ]; then
 	echo "Installing wordpress DATADIR="$WP_DATADIR
 	wp core download --path=$WP_DATADIR
-	export DB_PASSWORD=$(cat /run/secrets/db_password)
-	export WP_ADMIN_PASSWORD=$(cat /run/secrets/wp_admin_password)
 	wp config create --path=$WP_DATADIR \
 		--dbname=$WORDPRESS_DB --dbuser="$WP_DB_USER" \
-		--dbpass="$DB_PASSWORD" --dbhost="mariadb:3306"
-	wp core install --url=$HOSTNAME --path=$WP_DATADIR --title="Inception wordpress" \
-		--admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD \
-		--admin_email=test@test.com --skip-email
-	unset DB_PASSWORD
-	unset WP_ADMIN_PASSWORD
-	export WP_USER_PASSWORD = $(cat /run/secrets/wp_user_password)
-	wp user create $WP_USER test@example.com --role=subscriber --user_pass=secret --path=/var/www/html
-	echo "install END"
+		--dbpass="$(cat /run/secrets/db_password)" --dbhost=$DB_HOST
+	wp core install --url=$HOSTNAME --path=$WP_DATADIR --title="$wP_TITLE" \
+		--admin_user=$WP_ADMIN --admin_password=$(cat /run/secrets/wp_admin_password) \
+		--admin_email=$WP_ADMIN_EMAIL --skip-email
+	wp user create $WP_USER $WP_USER_EMAIL --role=subscriber \
+		--user_pass=$(cat /run/secrets/wp_user_password) --path=$WP_DATADIR
 fi
 
 exec php-fpm84 -F
